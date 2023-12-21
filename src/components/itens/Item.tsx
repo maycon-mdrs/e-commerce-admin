377e5f6828de466fe91a987a906fdbb272273834
Maycon Douglas <mdrs.maycon@gmail.com>
2023-12-21 00:10:01 -0300

    feat: empty imagem for product

--- src/components/itens/Item.tsx
+++ src/components/itens/Item.tsx
@@ -1,26 +1,28 @@
 import { Card } from "antd";
-import 'bootstrap/dist/css/bootstrap.css';
 import { IProduct } from "./types";
+import Empty from "../../images/empty.svg";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../style.css';
 
 export function Item({ productId, product}: { productId: string, product: IProduct }) {
     return (
         <Card style={{ width: '100%' }} key={productId} hoverable>
             <section style={{display: 'flex', width: 'auto', justifyContent: 'space-between'}}>
                 <div className="d-flex flex-row align-items-center">
-                    <img src={product.image} alt="" style={{width: 60, marginRight: '10px'}}/>
+                    <img src={product.image ?? Empty} alt="" style={{width: 60, marginRight: '10px'}}/>
                     <section style={{textAlign: 'start'}}>
-                        <h4>{product.title}</h4> {/* titulo */}
-                        <h6>{product.description}</h6> {/* descrição */}
+                        <h6 className="mb-0">{product.title}</h6> {/* titulo */}
+                        <p className="mb-0">{product.description}</p> {/* descrição */}
                     </section>
                 </div>
                 <div className="d-flex flex-row align-items-center" style={{gap: 20, textAlign: 'end'}}>
                     <section>
-                        <p>Valor</p>
-                        <h6>R${product.price?.toFixed(2)}</h6> {/* valor */}
+                        <p className="mb-0">Valor</p>
+                        <h6 className="mb-0">R${product.price?.toFixed(2)}</h6> {/* valor */}
                     </section>
                     <section>
-                        <p>Estoque</p>
-                        <h5>{product.quantity}</h5> {/* estoque */}
+                        <p className="mb-0">Estoque</p>
+                        <h6 className="mb-0">{product.quantity}</h6> {/* estoque */}
                     </section>
                 </div>
             </section>
